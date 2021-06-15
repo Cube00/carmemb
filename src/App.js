95d3fd0d0f78bb3b29952175cde7a803f99132f1
Your Name <coller.vato@gmail.com>
2021-06-15 10:36:12 +0400

    complete header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/header'
+
 function App() {
   return <>
+  <Router>
+    <Header />
+  </Router>
     this is carmemb
   </>
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,37 @@
+import {Link} from "react-router-dom";
+import Logo from '../assets/images/logo.png'
+import {BsBookmark} from 'react-icons/bs'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {RiSearch2Line} from 'react-icons/ri'
+
 const Header = () =>{
-  return 'header'
+  return <>
+    <header>
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="nav-b-ic-section">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <button className="booking-conv">
+          <BsBookmark className="booking-icon" />
+          BOOK A CONSULTATION
+        </button>
+        <span className="store-icon">
+          <span className="item-number">2</span>
+          <AiOutlineShoppingCart className="shop-icon" />
+        </span>
+        <button className="search"><RiSearch2Line/></button>
+      </div>
+    </header>
+  </>
 }
 
 export default Header
